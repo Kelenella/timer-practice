2e761f7d76c03ae943d6047eb677d7a45fae8734
Jane Yushchenko <kelenella@ukr.net>
2021-08-20 17:59:52 +0300

    add fn start and stop btn

--- src/index.js
+++ src/index.js
@@ -6,3 +6,28 @@ import './sass/main.scss';
 // for (let i = 3; i > 0; i--) {
 //     setTimeout(() => console.log(i), i * 1000);
 // }
+///////////////////////////////////////////////////////
+const refs = {
+    start: document.querySelector('#start'),
+    stop: document.querySelector('#stop')
+}
+// зафиксировать дату старта
+// посчитать сколько времени прошло от старта
+
+let id = null;
+let startDate = null;
+
+function logger() {
+    console.log('run');
+}
+function timerStart() {
+    startDate = Date.now()
+    id = setInterval(logger, 1000);
+    console.log(startDate);
+}
+function stopTimer() {
+    clearInterval(id)
+}
+
+refs.start.addEventListener('click', timerStart)
+refs.stop.addEventListener('click', stopTimer)
