2bef62248fce0ac1a0a139ee0c387fbf5c0ce285
Jane Yushchenko <kelenella@ukr.net>
2021-08-21 09:51:28 +0300

    made a Class for timer

--- src/index.js
+++ src/index.js
@@ -7,27 +7,80 @@ import './sass/main.scss';
 //     setTimeout(() => console.log(i), i * 1000);
 // }
 ///////////////////////////////////////////////////////
-const refs = {
-    start: document.querySelector('#start'),
-    stop: document.querySelector('#stop')
+
+class StopWatch {
+    constructor() {
+        this.refs = {
+            start: document.querySelector('#start'),
+            stop: document.querySelector('#stop'),
+            sec: document.querySelector('.seconds'),
+            min: document.querySelector('.minutes'),
+            hours: document.querySelector('.hours'),
+        };
+
+        this.id = null;
+        this.startDate = null;
+        this.timerStart = this.timerStart.bind(this)
+        this.timerStop = this.timerStop.bind(this)
+        
+    }
+    calc = () => {
+        const currentDate = Date.now();
+        const delta = currentDate - this.startDate;
+        const convertResult = delta / 1000;
+        const sec = Math.floor(convertResult % 60);
+        const min = Math.floor(convertResult / 60);
+        const hours = Math.floor(convertResult / 60 / 60);
+        console.log(hours);
+        this.refs.sec.textContent = sec < 10 ? `0${sec}` : sec;
+        this.refs.min.textContent = min < 10 ? `0${min}` : min;
+    }
+    timerStart = () => {
+        this.startDate = Date.now();
+        this.id = setInterval(this.calc, 1000);
+};
+    timerStop = () => {
+        clearInterval(this.id)
+};
+    init() {
+        this.refs.start.addEventListener('click', this.timerStart);
+        this.refs.stop.addEventListener('click', this.timerStop);
+    }
 }
+
+const watch = new StopWatch();
+watch.init();
+
+// const refs = {
+//     start: document.querySelector('#start'),
+//     stop: document.querySelector('#stop'),
+//     sec: document.querySelector('.seconds'),
+//     min: document.querySelector('.minutes'),
+// }
 // зафиксировать дату старта
 // посчитать сколько времени прошло от старта
 
-let id = null;
-let startDate = null;
+// let id = null;
+// let startDate = null;
 
-function logger() {
-    console.log('run');
-}
-function timerStart() {
-    startDate = Date.now()
-    id = setInterval(logger, 1000);
-    console.log(startDate);
-}
-function stopTimer() {
-    clearInterval(id)
-}
+// function calc() {
+//     const currentDate = Date.now();
+//     const delta = currentDate - startDate;
+//     const convertResult = delta / 1000;
+//     const sec = Math.floor(convertResult % 60);
+//     const min = Math.floor(convertResult / 60);
+//     refs.sec.textContent = sec < 10 ? `0${sec}` : sec;
+//     refs.min.textContent = min < 10 ? `0${min}` : min;
+//     // console.log(Math.floor(delta/1000));
+// }
 
-refs.start.addEventListener('click', timerStart)
-refs.stop.addEventListener('click', stopTimer)
+// function timerStart() {
+//     startDate = Date.now();
+//     id = setInterval(calc, 1000);
+// }
+// function timerStop() {
+//     clearInterval(id)
+// }
+
+// refs.start.addEventListener('click', timerStart)
+// refs.stop.addEventListener('click', timerStop)
